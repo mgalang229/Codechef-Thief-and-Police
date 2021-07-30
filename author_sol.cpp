b2ac389a598c999cb44acc86104014b8d1120b02
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-07-30 14:17:11 +0800

    Create author_sol.cpp

---
+++ author_sol.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n, m, x, y, a, b;
+		cin >> n >> m >> x >> y >> a >> b;
+		// calculate the distance of the thief using right or down moves
+		int thief_distance = (n - x) + (m - y);
+		// calculate the distance of the police by using the (right + down) and remaining moves
+		// minimum(right moves, down moves) = (right + down moves) (probability: sum rule)
+		int police_distance = (n - a) + (m - b) - min(n - a, m - b);
+		cout << (thief_distance <= police_distance ? "YES" : "NO") << '\n';
+	}
+	return 0;
+}
